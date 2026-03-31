1d52e82c00aa03d407cdf310a597d2c7a5df329f
gennarikun <gennarikun@gmail.com>
2026-04-01 06:47:00 +0900

    Initialize clean static site

---
+++ header.php
@@ -0,0 +1,31 @@
+<header class="siteHeader">
+		<div class="container siteHeadContainer">
+		<div class="navbar-header">
+						<p class="navbar-brand siteHeader_logo">
+			<a href="/">
+				<span><img src="/assets/390170.jpg" alt="Japanese cooking class FUJIYA @Kawaguchiko | Hoto,Udon, Soba experience"></span>
+			</a>
+			</p>
+					</div>
+
+					<div id="gMenu_outer" class="gMenu_outer">
+				<nav class="menu-a-container"><ul id="menu-a" class="menu gMenu vk-menu-acc"><li id="menu-item-2428" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/dantai/"><strong class="gMenu_name">Group Lunches</strong></a></li>
+<li id="menu-item-1528" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/access/"><strong class="gMenu_name">Location</strong></a></li>
+<li id="menu-item-2612" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/atelier/"><strong class="gMenu_name">Cat Cafe</strong></a></li>
+<li id="menu-item-2613" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/clothing-dyeing-experience/"><strong class="gMenu_name">Dyeing Experience</strong></a></li>
+<li id="menu-item-2614" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children"><a href="/menu/"><strong class="gMenu_name">Cooking Experience</strong></a>
+<ul class="sub-menu">
+	<li id="menu-item-2641" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/">Premium Plan</a></li>
+	<li id="menu-item-2615" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/no-meal-houtou-noodle-making-experience-shichimi-spice-making-experience-fabric-dyeing-experience-and-souvenir-making-experience-menu-guide/">No-Meal  Experience</a></li>
+</ul>
+</li>
+<li id="menu-item-2640" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/birthday/"><strong class="gMenu_name">Birthday Plan</strong></a></li>
+<li id="menu-item-1558" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/gallery/"><strong class="gMenu_name">Pictures</strong></a></li>
+<li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children"><a href="/contact/"><strong class="gMenu_name">Reservation</strong></a>
+<ul class="sub-menu">
+	<li id="menu-item-1566" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/rule/">Notice</a></li>
+</ul>
+</li>
+</ul></nav>			</div>
+			</div>
+	</header>
