951871d5fe21aea80ff1a4af5ca4a974e45cbda6
narasaki ryosei <narasaki@narasakinoMacBook-Air.local>
2026-04-02 02:25:09 +0300

    Fix: Remove birthday plan link, add banquet link, and remove redundant widgets

--- footer.php
+++ footer.php
@@ -2,11 +2,12 @@
 			<div class="footerMenu">
 			<div class="container">
 				<nav class="menu-a-container"><ul id="menu-a-1" class="menu nav"><li id="menu-item-2428" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2428"><a href="/dantai/">Group Lunches</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/izakaya/">Banquet / Private</a></li>
 <li id="menu-item-1528" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1528"><a href="/access/">Location</a></li>
 <li id="menu-item-2612" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2612"><a href="/atelier/">Cat Cafe</a></li>
 <li id="menu-item-2613" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2613"><a href="/clothing-dyeing-experience/">Dyeing Experience</a></li>
 <li id="menu-item-2614" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-2614"><a href="/menu/">Cooking Experience</a></li>
-<li id="menu-item-2640" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2640"><a href="/birthday/">Birthday Plan</a></li>
+
 <li id="menu-item-1558" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1558"><a href="/gallery/">Pictures</a></li>
 <li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="/contact/">Reservation</a></li>
 </ul></nav>			</div>
@@ -17,6 +18,7 @@
 			<p>Copyright © Japanese cooking class FUJIYA @Kawaguchiko | Hoto,Udon, Soba experience All Rights Reserved.</p><p>Powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://lightning.nagoya/ja/" target="_blank" title="Free WordPress Theme Lightning"> Lightning Theme</a> by Vektor,Inc. technology.</p>	</div>
 </footer>
 <div id="vk-mobile-nav-menu-btn" class="vk-mobile-nav-menu-btn">MENU</div><div class="vk-mobile-nav vk-mobile-nav-drop-in" id="vk-mobile-nav"><nav class="vk-mobile-nav-menu-outer" role="navigation"><ul id="menu-a-2" class="vk-menu-acc menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2428"><a href="/dantai/">Group Lunches</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/izakaya/">Banquet / Private</a></li>
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1528"><a href="/access/">Location</a></li>
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2612"><a href="/atelier/">Cat Cafe</a></li>
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2613"><a href="/clothing-dyeing-experience/">Dyeing Experience</a></li>
@@ -26,7 +28,7 @@
 	<li id="menu-item-2615" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2615"><a href="/no-meal-houtou-noodle-making-experience-shichimi-spice-making-experience-fabric-dyeing-experience-and-souvenir-making-experience-menu-guide/">No-Meal  Experience</a></li>
 </ul>
 </li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2640"><a href="/birthday/">Birthday Plan</a></li>
+
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1558"><a href="/gallery/">Pictures</a></li>
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-31"><a href="/contact/">Reservation</a>
 <ul class="sub-menu">
